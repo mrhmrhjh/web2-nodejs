435352f81fa6ec50a32585a6dc98fe93b9a0bf6b
kcsw1 <rndsw1@kcscopes.com>
2021-07-19 18:03:23 +0900

    * Javascript 배열, 반복문 예제

---
+++ syntax/array.js
@@ -0,0 +1,11 @@
+var arr = ['A','B','C','D', true]; //배열 생성
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
+
+// arr.pop('B');
+// console.log(arr);
