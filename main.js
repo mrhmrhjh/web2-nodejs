e8794cac28af0a303e8030c0e8bb645c1862a5cd
kcsw1 <rndsw1@kcscopes.com>
2021-07-19 18:59:29 +0900

    * App - 함수를 이용해서 정리 정돈하기 * javascript - 함수 예제

--- main.js
+++ main.js
@@ -2,6 +2,38 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+
+function templateHTML(title, list, body) {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+  `;
+
+}
+
+function templateList(fileList) {
+  var list = '<ul>';
+  var i = 0;
+  while ( i < fileList.length) {
+    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i = i+1;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     // console.log(_url);
@@ -36,29 +68,32 @@ var app = http.createServer(function(request,response){
             desc = 'Hello Node.js';
             title = 'Welcom';
           }
-          var list = '<ul>';
-          var i = 0;
-          while ( i < fileList.length) {
-            list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-            i = i+1;
-          }
-          list = list + '</ul>';
+          // var list = '<ul>';
+          // var i = 0;
+          // while ( i < fileList.length) {
+          //   list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          //   i = i+1;
+          // }
+          // list = list + '</ul>';
+          var list = templateList(fileList);
 
-          var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${desc}</p>
-            </body>
-            </html>
-          `;
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${desc}</p>`);
+          // var template = templateHTML(title, list, `<h2>${title}</h2>${desc}`);
+          //  `
+          //   <!doctype html>
+          //   <html>
+          //   <head>
+          //     <title>WEB1 - ${title}</title>
+          //     <meta charset="utf-8">
+          //   </head>
+          //   <body>
+          //     <h1><a href="/">WEB</a></h1>
+          //     ${list}
+          //     <h2>${title}</h2>
+          //     <p>${desc}</p>
+          //   </body>
+          //   </html>
+          // `;
           response.writeHead(200);
           response.end(template);
         });
