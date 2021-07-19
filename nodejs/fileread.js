307f84e60a1dc8250526b035901b881ce5515e6b
kcsw1 <rndsw1@kcscopes.com>
2021-07-19 16:20:00 +0900

    * 파일 읽기 예제

---
+++ nodejs/fileread.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+// fs.readFile('sample.txt', 'utf8', function(err, data){
+//   console.log(data);
+// });
+
+
+fs.readFile('sample.txt', 'utf8' ,(err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
