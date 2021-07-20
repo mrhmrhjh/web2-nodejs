ebaefdf75c14b0cf295fdc00655f05fe21e009e0
kcsw1 <rndsw1@kcscopes.com>
2021-07-20 18:58:15 +0900

    * App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -2,38 +2,39 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var template = require('./lib/template.js');
 
-var template = {
-  HTML : function (title, list, body, control) {
-    return `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB2</a></h1>
-        ${list}
-        ${control}
-        ${body}
-      </body>
-      </html>
-    `;
-  },
-  list : function (fileList) {
-    var list = '<ul>';
-    var i = 0;
-    while ( i < fileList.length) {
-      list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-      i = i+1;
-    }
-    list = list + '</ul>';
-
-    return list;
-  }
-}
-
+// var template = {
+//   HTML : function (title, list, body, control) {
+//     return `
+//       <!doctype html>
+//       <html>
+//       <head>
+//         <title>WEB1 - ${title}</title>
+//         <meta charset="utf-8">
+//       </head>
+//       <body>
+//         <h1><a href="/">WEB2</a></h1>
+//         ${list}
+//         ${control}
+//         ${body}
+//       </body>
+//       </html>
+//     `;
+//   },
+//   list : function (fileList) {
+//     var list = '<ul>';
+//     var i = 0;
+//     while ( i < fileList.length) {
+//       list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+//       i = i+1;
+//     }
+//     list = list + '</ul>';
+//
+//     return list;
+//   }
+// }
+//
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
